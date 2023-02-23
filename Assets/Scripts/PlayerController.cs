2a492912046c100e65353e9dc5f63f27dc219f42
MisterX013 <121223933+MisterX013@users.noreply.github.com>
2023-02-23 12:58:38 +0100

    commit4
    Laisser le véhicule se déplacer vers la gauche/droite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 
 {
-    public float speed = 20;
+    public float turnSpeed;
+    public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+
     }
 }
